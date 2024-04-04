cdc664fc43b6f559b3b19beec581d76d2685b92d
brogers <brogers@cpsc.us-central1-a.c.cpsc-department-server.internal>
2024-04-03 21:48:48 -0400

    Added database connection to edit.php and made .gitignore for credentials.php file

--- edit.php
+++ edit.php
@@ -13,16 +13,30 @@
         <button class="button disabled">Edit</button>
         <a href="login.php"><button>Login</button></a>
     </div>
+
+    <?php
+        include "credentials.php";
+        $servername = "localhost";
+        $db = "Arapaima";
+        $connection = mysqli_connect($servername, $username, $password, $db);
+
+        if (mysqli_connect_errno()) {
+            echo "<p>Failed to connect to the server</p>";
+        } else {
+            echo "<p>Connected to the server</p>";
+        }
+    ?>
+
 	<h2>Edit</h2>
-	<form>
+	<form action="edit.php" method="post">
 	    <table>
 		<tr>
-	    	    <td><label>Title</label></td>
+	        <td><label>Title</label></td>
 		    <td><input type="text" name="title"><br></td>
 		</tr>
-	        <tr>
+	    <tr>
 		    <td><label>Release Date</label></td>
-	            <td><input type="text" name="release"><br></td>
+	        <td><input type="text" name="release"><br></td>
 		</tr>
 		<tr>
 		    <td><label>Developer</label></td>
@@ -33,26 +47,26 @@
 		    <td><input type="text" name="publisher"><br></td>
 		</tr>
 		<tr>
-	            <td><label>Rating</label></td>
-	            <td><input type="text" name="rating"><br></td>
+	        <td><label>Rating</label></td>
+	        <td><input type="text" name="rating"><br></td>
 		</tr>
 		<tr>
-	            <td><label>Platforms</label></td>
-	            <td><input type="text" name="platform"><br></td>
+	        <td><label>Platforms</label></td>
+	        <td><input type="text" name="platform"><br></td>
 		</tr>
 		<tr>
-	            <td><label>Genres</label></td>
-	            <td><input type="text" name="genre"><br></td>
+	        <td><label>Genres</label></td>
+	        <td><input type="text" name="genre"><br></td>
 		</tr>
-            </table>
+        </table>
 	    <fieldset>
 		<legend>Modes</legend>
 		    <table>
 			<tr>
-	                    <td><label>Singleplayer</label></td>
+	            <td><label>Singleplayer</label></td>
 			    <td><input type="checkbox" name="singleplayer"><br></td>
 			    <td><label>Multiplayer</label></td>
-		            <td><input type="checkbox" name="multiplayer"><br></td>
+		        <td><input type="checkbox" name="multiplayer"><br></td>
 			</tr>
 		    </table>
 	    </fieldset>
